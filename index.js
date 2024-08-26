f1f9b1cbcd42023917904f245443b1ed8ba1cebc
harish04ss <harishsubramanian4122001@gmail.com>
2024-08-26 16:26:25 +0530

    basic server setup

---
+++ index.js
@@ -0,0 +1,8 @@
+//import http
+const http = require('http');
+const server = http.createServer((request,response)=>{
+    response.end('hello world');
+});
+server.listen(3001,'127.0.0.1',() =>{
+    console.log('server is runing on http://127.0.0.1:3001')
+});
