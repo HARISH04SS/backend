890da295feaee7854add82f9f1658945bbefdf22
harish04ss <harishsubramanian4122001@gmail.com>
2024-08-26 16:48:59 +0530

    express server is created

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-//import http
-const http = require('http');
-const server = http.createServer((request,response)=>{
-    response.end('hello world');
-});
-server.listen(3001,'127.0.0.1',() =>{
-    console.log('server is runing on http://127.0.0.1:3001')
-});
+const express = require('express');
+const app = express();
+app.get('/',(request,response)=>{
+    response.send("Hello world");
+})
+app.listen(3001,()=>{
+    console.log('server is running on 3001 at http://1270.0.1:3001');
+})
