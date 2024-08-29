5c98de270802c924c00ccce341202b64e8cdd39f
harish04ss <harishsubramanian4122001@gmail.com>
2024-08-30 02:26:17 +0530

    connect to atlas

--- index.js
+++ index.js
@@ -2,14 +2,15 @@
 const { error } = require('console');
 const http = require('http');
 const mongoose = require('mongoose');
+require('dotenv').config();
 const server = http.createServer((request,response)=>{
     response.end('hello world!!');
 });
 server.listen(3001,'127.0.0.1',() =>{
-    console.log('server is runing on http://127.0.0.1:3001/fsd56wee')
+    console.log('server is runing on http://127.0.0.1:3001')
 });
 
-mongoose.connect('mongodb://localhost:27017').then(()=>{
+mongoose.connect(process.env.mongoDB_URL).then(()=>{
     console.log("connected to DB");
 }).catch((error)=>{
     console.log("error in connection: ",error);
