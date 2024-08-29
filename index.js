fe6c4f13245c38c4872252f36f68f50c6465c858
harish04ss <harishsubramanian4122001@gmail.com>
2024-08-29 22:48:18 +0530

    connected to local DB

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 //import http
+const { error } = require('console');
 const http = require('http');
+const mongoose = require('mongoose');
 const server = http.createServer((request,response)=>{
     response.end('hello world!!');
 });
 server.listen(3001,'127.0.0.1',() =>{
-    console.log('server is runing on http://127.0.0.1:3001')
+    console.log('server is runing on http://127.0.0.1:3001/fsd56wee')
+});
+
+mongoose.connect('mongodb://localhost:27017').then(()=>{
+    console.log("connected to DB");
+}).catch((error)=>{
+    console.log("error in connection: ",error);
 });
