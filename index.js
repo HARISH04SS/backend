4deb55e3e1ab1620317b5bd16ad69695b5e47379
harish04ss <harishsubramanian4122001@gmail.com>
2024-08-29 22:15:47 +0530

    nodemon

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //import http
 const http = require('http');
 const server = http.createServer((request,response)=>{
-    response.end('hello world');
+    response.end('hello world!!');
 });
 server.listen(3001,'127.0.0.1',() =>{
     console.log('server is runing on http://127.0.0.1:3001')
