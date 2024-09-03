f57f5897b48f848caf57e59d8b5deb48fca3adcc
harish04ss <harishsubramanian4122001@gmail.com>
2024-09-03 23:14:37 +0530

    app started after db connected

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const app = require('./app')
 const mongoose = require('mongoose');
 const {mongoDB_URL,port} = require('./utils/config');
 
-app.listen(port,() =>{
-    console.log('server is runing on http://127.0.0.1:${process.env.port}')
-});
+
 
 mongoose.connect(mongoDB_URL).then(()=>{
     console.log("connected to DB");
+    app.listen(port,() =>{
+        console.log('server is runing on http://127.0.0.1:${port}')
+    });
 }).catch((error)=>{
     console.log("error in connection: ",error);
 });
